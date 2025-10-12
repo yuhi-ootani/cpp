977cc0783e7b5e59fc78740d978f416d42791341
Otani Yuhi <oyuhi@c4r3s18.42tokyo.jp>
2025-10-12 19:38:54 +0900

    adfdas

--- cpp9/ex02/PmergeMe.cpp
+++ cpp9/ex02/PmergeMe.cpp
@@ -1,6 +1,21 @@
 
 #include "PmergeMe.hpp"
 
+long jacobsthal_number(long n) {
+    if (n <= 0)
+        return 0; // J0 = 0
+    if (n == 1)
+        return 1; // J1 = 1
+    long a = 0;   // J0
+    long b = 1;   // J1
+    for (long i = 2; i <= n; ++i) {
+        long c = b + 2 * a; // Jn = J(n-1) + 2*J(n-2)
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
 PmergeMe::PmergeMe() : _vec(), _deque() {}
 
 PmergeMe::PmergeMe(const std::vector<int> &before)
@@ -18,4 +33,4 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
 
 PmergeMe::~PmergeMe() {}
 
-
+void PmergeMe::sortVec() { size_t block_size = biggest_block_sort(_vec); }
--- cpp9/ex02/PmergeMe.hpp
+++ cpp9/ex02/PmergeMe.hpp
@@ -2,27 +2,59 @@
 #ifndef PMERGEME
 #define PMERGEME
 
-#include <deque>
-#include <iostream>
-#include <string>
-#include <set>
-#include <vector>
-#include <cstdlib>
 #include <cerrno>
 #include <climits>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
 
 class PmergeMe {
   private:
-   
     std::vector<int> _vec;
     std::deque<int> _deque;
 
   public:
     PmergeMe();
-    PmergeMe(const std::vector<int>& before);
+    PmergeMe(const std::vector<int> &before);
     PmergeMe(const PmergeMe &other);
     PmergeMe &operator=(const PmergeMe &other);
     ~PmergeMe();
+
+    void sortVec();
+    void sortDeque();
 };
 
 #endif
+
+template <typename Container>
+
+// L 1 2 M 4 5 R;
+static inline void swap_bigger_block(Container &c, size_t L, size_t M, size_t R) {
+    if (c[R - 1] < c[M - 1])
+        std::rotate(c.begin() + L, c.begin() + M, c.begin() + R);
+}
+
+template <typename Container>
+
+static inline size_t biggest_block_sort(Container &c) {
+    const size_t elements = c.size();
+    if (elements < 2)
+        return 1;
+
+    size_t block_size = 1;
+    for (; (block_size << 1) <= elements; block_size *= 2) {
+        size_t full_blocks = elements / block_size;
+        const size_t end_block = (full_blocks & 1) ? full_blocks - 1 : full_blocks;
+
+        for (size_t cur_block = 0; cur_block + 1 < end_block; cur_block += 2) {
+            size_t L = cur_block * block_size;
+            size_t M = L + block_size;
+            size_t R = M + block_size;
+            swap_bigger_block(c, L, M, R);
+        }
+    }
+    return block_size;
+}
